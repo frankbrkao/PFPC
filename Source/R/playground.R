dd400448a1b0fef3200d1cc3d2a5fe5962e6a4d4
Frank Kao <frank_kao@htc.com>
2017-11-17 02:10:27 +0800

    [PFPC] Update predict & log function for new table
    * Prediction
      - randomForest_all
      - randomForest_city
    
    * Performance evaluation & logging
      - evaluate_per_type
      - save_performance
      - save_submit

--- Source/R/playground.R
+++ Source/R/playground.R
@@ -6,7 +6,6 @@
 # install.packages("randomForest")
 # install.packages("rJava")
 # install.packages("reshape")
-# install.packages("tictoc")
 
 # clean all objects from workspace
 rm(list=ls())
@@ -19,7 +18,6 @@ library(tidyverse)
 library(rJava)
 library(randomForest)
 library(reshape)
-library(tictoc)
 source("./R/util.R")
 source("./R/data.R")
 
@@ -31,68 +29,18 @@ source("./R/data.R")
 # gen_meters_info()
 # gen_village_info()
 # gen_station_observation()
-# merge_all_info()
-
-# =================================================================================================
-# Data pre-processing
+# merge_all_info(f_last_submit="59.01400_submit_dc_1112_233124.csv")
 
 # =================================================================================================
 
-# md = data_pre_processing()
-# 
-# data = md$data
-# info = md$info
-
-# =================================================================================================
-
-data = list()
-data$train  = read.csv("./data/train.csv",  fileEncoding="UTF-8")
-data$submit = read.csv("./data/submit.csv", fileEncoding="UTF-8")
-data$all    = read.csv("./data/merged.csv", fileEncoding="UTF-8")
-
-# =================================================================================================
-
-info = list()
-info$time_stamp = format(Sys.time(), "%m%d_%H%M%S")
-info$tn_tp = c("Hagibis", "Chan.hom", "Dujuan", "Soudelor", "Fung.wong", "Matmo", "Nepartak", "MerantiAndMalakas")
-info$ts_tp = c("NesatAndHaitang", "Megi")
-info$tp = c(info$tn_tp, info$ts_tp)
-
-features = c("pole1", "pole2", "pole3", "pole4", "pole5", "pole6", "pole7", "pole8", "pole9", "pole10")
-features = c(features, "precp_total", "precp_day", "precp_24h", "precp_12h", "precp_6h", "precp_3h", "precp_1h", "wind", "gust")
-features = c(features, "max_hh")
-info$features = features
-
-info$cities = levels(data$train$CityName)
-
-info$col_tn = c(info$features, "outage")
-info$row_tn = which(data$all$tp %in% info$tn_tp)
-info$row_ts = which(data$all$tp %in% info$ts_tp)
-
-data$tn = data$all[info$row_tn,]
+data = gen_data()
+info = gen_info()
 
 # =================================================================================================
 # Build randomforest via all data
-
-st = Sys.time()
-
-md = list()
-md$real = data$all$outage
-md$pred = rep(0, length(md$real))
-
-# city = info$cities[1]
-md$model = randomForest(outage~., data=data$all[info$row_tn, info$col_tn], ntree=500)
-md$pred  = round(predict(md$model, newdata=data$all[, info$features]))
-
-cm = CM(md$real[info$row_tn], md$pred[info$row_tn])
-duration = Sys.time() - st
-message(sprintf(" total - rows: %5d, cm: %2.6f, duration: %6.2fs", length(info$row_tn), cm, duration))
-
-save_submit(md$real, md$pred, diff=T)
-save_model(f_prefix="rf_all_", model=md$model)
+# randomForest_all()
 
 # =================================================================================================
-
 randomForest_city()
 
 # =================================================================================================
--- Source/R/util.R
+++ Source/R/util.R
@@ -1,5 +1,58 @@
 # =================================================================================================
 
+gen_data = function() {
+
+    data = list()
+    data$train  = read.csv("./data/train.csv",  fileEncoding="UTF-8")
+    data$submit = read.csv("./data/submit.csv", fileEncoding="UTF-8")
+    data$ref    = read.csv("./data/ref.csv",    fileEncoding="UTF-8")
+    data$all    = read.csv("./data/merged.csv", fileEncoding="UTF-8")
+
+    data$all$Towns = paste0(data$all$CityName, data$all$TownName) %>% as.factor()
+    data$all$Vils  = paste0(data$all$CityName, data$all$TownName, data$all$VilName) %>% as.factor()
+
+    return (data)
+}
+
+gen_info = function() {
+
+    info = list()
+    info$time_stamp = format(Sys.time(), "%m%d_%H%M%S")
+    info$tn_tp = c("Hagibis", "Chan.hom", "Dujuan", "Soudelor", "Fung.wong", "Matmo", "Nepartak", "MerantiAndMalakas")
+    info$ts_tp = c("NesatAndHaitang", "Megi")
+    info$tp = c(info$tn_tp, info$ts_tp)
+
+    features = c("pole1", "pole2", "pole3", "pole4", "pole5", "pole6", "pole7", "pole8", "pole9", "pole10")
+    features = c(features, "precp_day", "precp_24h", "precp_12h", "precp_6h", "precp_3h", "precp_1h", "wind", "gust")
+    # features = c(features, "precp_day", "wind", "gust")
+    features = c(features, "max_hh")
+    info$features = features
+
+    info$cities   = levels(data$train$CityName)
+    info$towns    = levels(data$all$Towns)
+    info$villages = levels(data$all$Vils)
+
+    info$col_tn = c(info$features, "outage")
+    info$row_tn = which(data$all$tp %in% info$tn_tp)
+    info$row_ts = which(data$all$tp %in% info$ts_tp)
+
+    info$tn_ratio = 0.8
+
+    info$pd_path = paste0(getwd(), "/prediction/")
+    if ( !dir.exists(info$pd_path) ) {
+        dir.create(info$pd_path)
+    }
+
+    info$md_path = paste0(getwd(), "/model/")
+    if ( !dir.exists(info$md_path) ) {
+        dir.create(info$md_path)
+    }
+
+    return (info)
+}
+
+# =================================================================================================
+
 data_pre_processing = function() {
     # 載入颱風停電戶資料
 
@@ -84,6 +137,123 @@ collect_info = function(train, lastpd) {
 
 # =================================================================================================
 
+randomForest_HL = function(save_model=T) {
+
+    st = Sys.time()
+
+    md = list()
+
+    info$H = 500
+
+    data$all$H = 0
+    sel_row = which(data$all$outage > info$H)
+    data$all$H[sel_row] = 1
+    data$all$H = as.factor(data$all$H)
+
+    md$real = data$all$H
+    md$pred = rep(0, length(md$real))
+
+    col_tn = c(info$features, "H")
+    raw_tn = data$all[info$row_tn,]
+
+    scope_H = intersect(which(data$all$outage > info$H), info$row_tn)
+    scope_L = intersect(which(data$all$outage <= info$H), info$row_tn)
+    len_H   = length(scope_H)
+    len_L   = length(scope_L)
+
+    ratio_tn = 0.85
+
+    idx_H = sample(1:len_H, len_H*ratio_tn, replace=F)
+    tn_H = scope_H[idx_H]
+
+    idx_L = sample(1:len_L, len_H*ratio_tn, replace=F)
+    tn_L = scope_L[idx_L]
+
+    sel_row = cbind(tn_H, tn_L)
+    row_ts  = setdiff(info$row_tn, sel_row)
+
+    md$model = randomForest(H~., data=data$all[sel_row, col_tn], ntree=500)    
+    md$pred  = predict(md$model, newdata=data$all[, info$features])
+
+    md$score_tn = (sum(md$pred[sel_row] == md$real[sel_row]) / length(sel_row))
+    md$score_ts = (sum(md$pred[row_ts] == md$real[row_ts]) / length(row_ts))
+
+    tn_len = length(sel_row)
+    ts_len = length(row_ts)
+    tn_err = (1-md$score_tn) * length(sel_row)
+    ts_err = (1-md$score_ts) * length(row_ts)
+
+    tn_score = md$score_tn * 100
+    ts_score = md$score_ts * 100
+
+    message(sprintf("tn : %2.4f (%6.0f / %6.0f), ts : %2.4f (%6.0f / %6.0f)", tn_score, tn_err, tn_len, ts_score, ts_err, ts_len))
+
+    tn_H_len = length(tn_H)
+    tn_L_len = length(tn_L)
+
+    md$score_tn_H = (sum(md$pred[tn_H] == md$real[tn_H]) / tn_H_len)
+    md$score_tn_L = (sum(md$pred[tn_L] == md$real[tn_L]) / tn_L_len)
+
+    tn_H_err = (1-md$score_tn_H) * tn_H_len
+    tn_L_err = (1-md$score_tn_L) * tn_L_len
+
+    tn_H_score = md$score_tn_H * 100
+    tn_L_score = md$score_tn_L * 100
+
+    message(sprintf("tnH: %2.4f (%6.0f / %6.0f), tnL: %2.4f (%6.0f / %6.0f)", tn_H_score, tn_H_err, tn_H_len, tn_L_score, tn_L_err, tn_L_len))
+
+    ts_H = setdiff(scope_H, tn_H)
+    ts_L = setdiff(scope_L, tn_L)
+
+    ts_H_len = length(ts_H)
+    ts_L_len = length(ts_L)
+
+    md$score_ts_H = (sum(md$pred[ts_H] == md$real[ts_H]) / ts_H_len)
+    md$score_ts_L = (sum(md$pred[ts_L] == md$real[ts_L]) / ts_L_len)
+
+    ts_H_err = (1-md$score_ts_H) * ts_H_len
+    ts_L_err = (1-md$score_ts_L) * ts_L_len
+
+    ts_H_score = md$score_ts_H * 100
+    ts_L_score = md$score_ts_L * 100
+
+    message(sprintf("tnH: %2.4f (%6.0f / %6.0f), tnL: %2.4f (%6.0f / %6.0f)", ts_H_score, ts_H_err, ts_H_len, ts_L_score, ts_L_err, ts_L_len))    
+
+    return (md)
+}
+
+# =================================================================================================
+# Build randomforest via all data
+
+randomForest_all = function(save_model=T) {
+
+    st = Sys.time()
+    
+    md = list()
+    md$real = data$all$outage
+    md$pred = rep(0, length(md$real))
+    
+    # city = info$cities[1]
+    
+    # sel_row = which(data$all$outage < info$limit_outage)
+    # sel_row = intersect(sel_row, info$row_tn)
+    # md$model = randomForest(outage~., data=data$all[sel_row, info$col_tn], ntree=500)
+    
+    md$model = randomForest(outage~., data=data$all[info$row_tn, info$col_tn], ntree=500)    
+    md$pred  = round(predict(md$model, newdata=data$all[, info$features]))
+    
+    cm = CM(md$real[info$row_tn], md$pred[info$row_tn])
+    duration = difftime(Sys.time(), st, units="sec")
+    message(sprintf(" total - rows: %5d, cm: %2.6f, duration: %6.2fs", length(info$row_tn), cm, duration))
+    
+    save_submit(md$real, md$pred, diff=T)
+    save_performance(model=md)
+    save_model(f_prefix="rf_all_", model=md$model)
+}
+
+# =================================================================================================
+# Build randomforest model per city
+
 randomForest_city = function(save_model=T) {
 
     st = Sys.time()
@@ -97,61 +267,131 @@ randomForest_city = function(save_model=T) {
         build_md_st = Sys.time()
 
         # city = info$cities[1]
-        raw = filter(data$tn, CityName == city)
-        rf  = randomForest(outage~., data=raw[, info$col_tn], ntree=500)
+        raw = filter(data$all[info$row_tn,], CityName == city)
 
         row_city = which(data$all$CityName == city)
+        row_raw  = intersect(row_city, info$row_tn)
+        raw_len  = nrow(raw)
+
+        tn_len   = as.integer(info$tn_ratio * raw_len)
+        sampling_idx = sample(1:raw_len, replace=F)
+        
+        row_tn = row_raw[sampling_idx[1:tn_len]]
+        row_vd = row_raw[sampling_idx[(tn_len+1):raw_len]]
+
+        rf  = randomForest(outage~., data=data$all[row_tn, info$col_tn], ntree=500)
         md$pred[row_city] = round(predict(rf, newdata=data$all[row_city, info$features]))
-
-        sel_row = intersect(info$row_tn, row_city)
-        cm = CM(md$real[sel_row], md$pred[sel_row])
-
+        
+        tn_cm = CM(md$real[row_tn], md$pred[row_tn]) * 100
+        vd_cm = CM(md$real[row_vd], md$pred[row_vd]) * 100
+        
         md[[city]] = rf
 
-        duration = Sys.time() - build_md_st
-        message(sprintf("%8s - rows: %5d, cm: %2.6f, duration: %6.2fs", city, length(row_city), cm, duration))
+        duration = difftime(Sys.time(), build_md_st, units="sec")
+        message(sprintf("%8s - rows: %5d / %5d, tn_cm: %2.6f, vd_cm: %2.6f, duration: %6.2fs", city, tn_len, raw_len, tn_cm, vd_cm, duration))
+        # message(sprintf("%8s - rows: %5d, duration: %6.2fs", city, length(row_city), duration))
     }
 
     cm = CM(md$real[info$row_tn], md$pred[info$row_tn])
-    duration = Sys.time() - st
-    message(sprintf(" total - rows: %5d, cm: %2.6f, duration: %6.2fs", length(info$row_tn), cm, duration))
-
+    duration = difftime(Sys.time(), st, units="sec")
+    message(sprintf(" total - rows: %5d, duration: %6.2fs", length(info$row_tn), duration))
+    
+    info$time_stamp = format(Sys.time(), "%m%d_%H%M%S")
+    
     save_submit(md$real, md$pred, diff=T)
+    save_performance(model=md)
     save_model(f_prefix="rf_city_", model=md)
 }
 
 # =================================================================================================
 
 save_submit = function(real, pred, diff=F) {
-
+    
+    # real     = model$real
+    # pred     = model$pred
+    
     submit   = data$submit
     sel_cols = colnames(submit)
+    ref      = data$ref
 
+    log = NULL
+    message("Score - typhoon")
+    log = rbind(log, "Score - typhoon")
+    
     for (tp in info$ts_tp) {
         row_tp = which(data$all$tp == tp)
         submit[,tp] = pred[row_tp]
         submit[,paste0(tp,"_last")] = real[row_tp]
 
         cm = CM(real[row_tp], pred[row_tp])
-        message(sprintf("%20s: %6.2f", tp, cm))
+        message(sprintf("%20s: %6.6f", tp, cm))
+        log = rbind(log, sprintf("%20s: %6.6f", tp, cm))
     }
 
-    pd_path = paste(c(getwd(), "/prediction/"), collapse='')
-    if ( !dir.exists(pd_path) ) {
-        dir.create(pd_path)
-    }
+    # =============================================================================================
+    
+    grp = group_by(submit, CityName) %>% 
+        summarise(NesatAndHaitang_pd = sum(NesatAndHaitang), Megi_pd = sum(Megi))
 
-    f_submit = paste(c(pd_path, "submit_", format(Sys.time(), "%m%d_%H%M%S"), ".csv"), collapse='')
+    # tp_total = list()
+    # tp_total["NesatAndHaitang"] = as.integer(672000)
+    # tp_total["Megi"] = as.integer(4206000)
+
+    result = left_join(ref, grp, by=c("City" = "CityName"))
+    
+    row_total = data_frame("Total", as.integer(672000), as.integer(4206000), sum(result[,"NesatAndHaitang_pd"]), sum(result[,"Megi_pd"]))
+    colnames(row_total) = colnames(result)
+    result = rbind(row_total, result)
+
+    message("Score - city outage")
+    log = rbind(log, "Score - city outage")
+    
+    row_city = (2:nrow(result))
+    
+    result = transform(result, NesatAndHaitang_pd=as.integer(NesatAndHaitang_pd))
+    result = transform(result, Megi_pd=as.integer(Megi_pd))
+    
+    for (tp in info$ts_tp) {
+        
+        tp_pd    = paste0(tp, "_pd")
+        tp_df    = paste0(tp, "_df")
+        tp_ratio = paste0(tp, "_ratio")
+        
+        result[,tp_df]    = result[,tp_pd] - result[,tp]
+        result[,tp_ratio] = (result[,tp_df] / result[,tp]) * 100
+        
+        cm = CM(result[row_city, tp], result[row_city, tp_pd])
+        
+        tl_real  = result[1, tp]
+        tl_pred  = result[1, tp_pd]
+        tl_df    = tl_pred - tl_real
+        tl_ratio = tl_df / tl_real
+        
+        message(sprintf("%20s - cm: %6.6f, total: %7d / %7d, diff: %d, ratio: %2.4f", tp, cm, tl_real, tl_pred, tl_df, tl_ratio))
+        log = rbind(log, sprintf("%20s - cm: %6.6f, total: %7d / %7d, diff: %8d, ratio: %2.4f", tp, cm, tl_real, tl_pred, tl_df, tl_ratio))
+    }    
+    
+    print(result)
+    
+    # =============================================================================================    
+        
+    f_submit = paste0(info$pd_path, "submit_", info$time_stamp, ".csv")
     write.csv(submit[, sel_cols], file=f_submit, row.names=FALSE, fileEncoding="UTF-8")
-    message(sprintf("save to %s", f_submit))
-
+    message(sprintf("save submit: %s", f_submit))
+    
+    f_submit = paste0(info$pd_path, "submit_", info$time_stamp, "_score.csv")
+    write.csv(result, file=f_submit, row.names=FALSE, fileEncoding="UTF-8")
+    write(log,    file=f_submit, append=TRUE)
+    # write(result, file=f_submit, append=TRUE)
+    message(sprintf("save  score: %s", f_submit))
+    
     if (!diff) {
         return (0)
     }
 
-    f_submit = paste(c(pd_path, "submit_", format(Sys.time(), "%m%d_%H%M%S"), "_diff", ".csv"), collapse='')
+    f_submit = paste0(info$pd_path, "submit_", info$time_stamp, "_diff.csv")
     write.csv(submit, file=f_submit, row.names=FALSE, fileEncoding="UTF-8")
-    message(sprintf("save diff to %s", f_submit))
+    message(sprintf("save   diff: %s", f_submit))
 }
 
 # =================================================================================================
@@ -159,17 +399,83 @@ save_submit = function(real, pred, diff=F) {
 save_model = function(f_prefix, model) {
 
     st = Sys.time()
-    md_path = paste(c(getwd(), "/model/"), collapse='')
-    if ( !dir.exists(md_path) ) {
-        dir.create(md_path)
-    }
 
-    f_md = paste(c(md_path, f_prefix, format(Sys.time(), "%m%d_%H%M%S"), ".md"), collapse='')
+    f_md = paste0(info$md_path, f_prefix, info$time_stamp, ".md")
     save(model, file=f_md)
 
+    duration = difftime(Sys.time(), st, units="sec")
+    message(sprintf("save  model: %s", f_md))
+    message(sprintf("duration of saving a model: %2.2fs", duration))
+}
+
+# =================================================================================================
+
+evaluate_per_type = function(model, type_name, type_idx, type_set) {
+    
+    message(sprintf("==== Evaluate performance per %s ====", type_name))
+    
+    result = NULL
+    
+    for (type in type_set) {
+        
+        row_type = which(data$all[, type_idx] == type)
+        sel_row  = intersect(info$row_tn, row_type)
+        cm = CM(model$real[sel_row], model$pred[sel_row])
+
+        result = rbind(result, sprintf("%30s: %2.6f", type, cm))
+        # message(sprintf("%8s: %2.6f", type, cm))
+    }
+    
+    return (result)
+}
+
+# =================================================================================================
+
+save_performance = function(model) {
+
+    st = Sys.time()
+    
+    result = NULL
+    result = rbind(result, "=====================================================")
+    result = rbind(result, evaluate_per_type(model=model, type_name="typhoon", type_idx="tp",       type_set=info$tn_tp))
+    result = rbind(result, "=====================================================")
+    result = rbind(result, evaluate_per_type(model=model, type_name="city",    type_idx="CityName", type_set=info$cities))
+    result = rbind(result, "=====================================================")
+    result = rbind(result, evaluate_per_type(model=model, type_name="town",    type_idx="Towns",    type_set=info$towns))
+    result = rbind(result, "=====================================================")
+    result = rbind(result, evaluate_per_type(model=model, type_name="village", type_idx="Vils",     type_set=info$villages))
+    result = rbind(result, "=====================================================")
+    
+    cm = CM(model$real[info$row_tn], model$pred[info$row_tn])
+    result = rbind(result, sprintf("total: %2.6f", cm))
+    
     duration = Sys.time() - st
-    message(sprintf("save model: %s", f_md))
-    message(sprintf("duration: %2.2fs", duration))
+    message(sprintf(" total - rows: %5d, cm: %2.6f, duration: %6.2fs", length(info$row_tn), cm, duration))
+    
+    result = rbind(result, "=====================================================")
+    
+    # =================================================================================================
+    
+    for (tp in info$ts_tp) {
+        
+        row_tp = which(data$all$tp == tp)
+        cm = CM(model$real[row_tp], model$pred[row_tp])
+        result = rbind(result, sprintf("%20s: %6.2f", tp, cm))
+    }
+    
+    # =================================================================================================
+    
+    f_submit = paste0(info$pd_path, "submit_", info$time_stamp, "_pf.csv")
+    write.csv(result, file=f_submit, row.names=FALSE, fileEncoding="UTF-8")
+    message(sprintf("save submit: %s", f_submit))
+    
+    # =================================================================================================
+    
+    sel_col   = c("CityName", "TownName", "VilName", "VilCode", "key", "tp", "outage")
+    pf_detail = cbind(data$all[, sel_col], model$pred)
+    f_submit = paste0(info$pd_path, "submit_", info$time_stamp, "_pf_detail.csv")
+    write.csv(pf_detail, file=f_submit, row.names=FALSE, fileEncoding="UTF-8")
+    message(sprintf("save detail: %s", f_submit))
 }
 
 # =================================================================================================
